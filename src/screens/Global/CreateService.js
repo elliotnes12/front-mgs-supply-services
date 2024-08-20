f937a171f4546b0c0dcc96d51b779f42d791d93c
elliot <pereznazario21@gmail.com>
2024-08-19 18:04:24 -0600

    se agrega funcionalidadde nuevo servicio

--- src/screens/Global/CreateService.js
+++ src/screens/Global/CreateService.js
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { Text, TextInput, TouchableOpacity, View, Alert, ActivityIndicator, ScrollView } from "react-native";
+import {
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Alert,
+  ActivityIndicator,
+  ScrollView,
+  Dimensions,
+} from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import Modal from "react-native-modal";
 import * as Location from "expo-location";
@@ -8,8 +17,11 @@ import { assets } from "../../assets";
 import { styles } from "./styles/CreateService.style";
 import { getIconById } from "../../utils/util";
 import { Header } from "../../components/core/Header";
-import StyledText from "../../utils/globalstyle";
+import StyledText, { StyledGradientButtonSmall } from "../../utils/globalstyle";
 import { stylesGlobal } from "../../modules/styles/global.style";
+import { SceneMap, TabBar, TabView } from "react-native-tab-view";
+import { Calendar } from "react-native-calendars";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 const data = [
   { id: "1", title: "Cleaning" },
@@ -23,16 +35,27 @@ const dataEmployees = [
 ];
 
 export function CreateService() {
+  const initialLayout = { width: Dimensions.get("window").width };
   const [selectedId, setSelectedId] = useState(data[0].id);
-  const [location, setLocation] = useState("Av. 3 Calle 4 with corner 24, reference cc Rodeo");
-  const [bussinessName, setBussinessName] = useState("CORPORATION VILLA NUEVA LLC");
+  const [location, setLocation] = useState(
+    "Av. 3 Calle 4 with corner 24, reference cc Rodeo"
+  );
+  const [bussinessName, setBussinessName] = useState(
+    "CORPORATION VILLA NUEVA LLC"
+  );
   const [searchText, setSearchText] = useState("");
   const [filteredEmployees, setFilteredEmployees] = useState(dataEmployees);
   const [loading, setLoading] = useState(true);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [origin, setOrigin] = useState(null);
-  const [selectedAddress, setSelectedAddress] = useState("Aquí va la dirección");
+  const [selectedAddress, setSelectedAddress] = useState(
+    "Aquí va la dirección"
+  );
   const [mapClicked, setMapClicked] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [isCalendarVisible, setIsCalendarVisible] = useState(false);
+  const [selectedTime, setSelectedTime] = useState(null);
+  const [isTimePickerVisible, setIsTimePickerVisible] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -71,11 +94,14 @@ export function CreateService() {
       const { latitude, longitude } = e.nativeEvent.coordinate;
       setOrigin({
         latitude: latitude,
-        longitude: longitude
-      })
+        longitude: longitude,
+      });
 
       try {
-        const geocode = await Location.reverseGeocodeAsync({ latitude, longitude });
+        const geocode = await Location.reverseGeocodeAsync({
+          latitude,
+          longitude,
+        });
 
         if (geocode.length > 0) {
           const { street, city, region, postalCode } = geocode[0];
@@ -90,14 +116,41 @@ export function CreateService() {
     }
   };
 
-  const getImage = (label) => {
-    if (label === "Cleaning") {
-      return assets.image.png.iconEscoba;
-    } else if (label === "Painting") {
-      return assets.image.png.iconBotePintura;
-    } else if (label === "Polishing") {
-      return assets.image.png.iconBotePintura;
+  const handleDateChange = (date) => {
+    setSelectedDate(date.dateString);
+    setIsCalendarVisible(false);
+  };
+
+  const toggleCalendarVisibility = () => {
+    setIsCalendarVisible(!isCalendarVisible);
+  };
+
+  const formatDate = (dateString) => {
+    if (!dateString) return "Choose a date";
+    const date = new Date(dateString);
+    const options = { day: "numeric", month: "short" };
+    return date.toLocaleDateString("en-US", options);
+  };
+
+  const handleTimeChange = (event, selectedDate) => {
+    setIsTimePickerVisible(Platform.OS === "ios");
+    if (selectedDate) {
+      const currentTime = new Date(selectedDate);
+      currentTime.setSeconds(0);
+      setSelectedTime(currentTime);
     }
+  };
+  const showTimePicker = () => {
+    setIsTimePickerVisible(true);
+  };
+
+  const formatTime = (date) => {
+    if (!date) return "Schedule";
+    return date.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    });
   };
 
   const handleSearch = () => {
@@ -108,122 +161,236 @@ export function CreateService() {
     );
     setFilteredEmployees(results);
   };
+  const cleaningRut = () => (
+    <ScrollView>
+      <View>
+        <View style={{ marginTop: 20, paddingHorizontal: 24 }}>
+          <StyledText headerGray style={[styles.titleServices]}>
+            Set date & time
+          </StyledText>
+
+          <TouchableOpacity
+            style={styles.item}
+            onPress={toggleCalendarVisibility}
+          >
+            <View style={{ width: 40, height: 40, marginRight: 10 }}>
+              {getIconById("calendar")}
+            </View>
+            <Text>{formatDate(selectedDate)}</Text>
+          </TouchableOpacity>
+          {isCalendarVisible && (
+            <View style={[styles.item, { zIndex: 1 }]}>
+              <Calendar
+                onDayPress={handleDateChange}
+                markedDates={{
+                  [selectedDate]: { selected: true, selectedColor: "#CEDC39" },
+                }}
+                style={{ borderRadius: 10, padding: 0 }}
+                theme={{
+                  backgroundColor: "black",
+                  calendarBackground: "#ffffff",
+                  textSectionTitleColor: "#CEDC39",
+                  selectedDayBackgroundColor: "#7DA74D",
+                  selectedDayTextColor: "#ffffff",
+                  todayTextColor: "#CEDC39",
+                  dayTextColor: "#7DA74D",
+                  textDisabledColor: "#ABABAB",
+                  dotColor: "#7DA74D",
+                  selectedDotColor: "#ffffff",
+                  arrowColor: "orange",
+                  monthTextColor: "#7DA74D",
+                  indicatorColor: "#7DA74D",
+                  textDayFontFamily: "monospace",
+                  textMonthFontFamily: "monospace",
+                  textDayHeaderFontFamily: "monospace",
+                  textDayFontWeight: "300",
+                  textMonthFontWeight: "bold",
+                  textDayHeaderFontWeight: "300",
+                  textDayFontSize: 16,
+                  textMonthFontSize: 16,
+                  textDayHeaderFontSize: 16,
+                }}
+              />
+            </View>
+          )}
+
+          <TouchableOpacity style={styles.item} onPress={showTimePicker}>
+            <View style={{ width: 40, height: 40, marginRight: 10 }}>
+              {getIconById("icontime")}
+            </View>
+            <Text>{formatTime(selectedTime)}</Text>
+          </TouchableOpacity>
+
+          {isTimePickerVisible && (
+            <DateTimePicker
+              value={selectedTime || new Date()}
+              mode="time"
+              is24Hour={false}
+              display="default"
+              onChange={handleTimeChange}
+            />
+          )}
+          <TouchableOpacity style={[styles.item]}>
+            <View
+              style={{
+                width: 30,
+                height: 30,
+                marginRight: 15,
+                marginLeft: 7,
+              }}
+            >
+              {getIconById("iconLupa")}
+            </View>
+            <Text>Search by name or id employee</Text>
+          </TouchableOpacity>
+          <View style={styles.employees}>
+            <Text style={[styles.titleServices, { marginBottom: 5 }]}>
+              Employees
+            </Text>
+          </View>
+          <StyledText
+            headerGray
+            style={[styles.textGray, styles.titleServices]}
+          >
+            Service Location
+          </StyledText>
+          <View style={[styles.item, stylesGlobal.itemVertical]}>
+            <Text>{selectedAddress}</Text>
+            <TouchableOpacity onPress={toggleModal}>
+              <Text style={styles.street__googleMaps}>SET GOOGLE MAP</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.bussiness}>
+            <Text style={styles.bussiness__title}>Bussiness Details</Text>
+            <Text style={[styles.textGray, styles.titleServices]}>
+              Bussiness Name
+            </Text>
+            <Text style={styles.bussiness__name}>{bussinessName}</Text>
+
+            <Text style={[styles.textGray, styles.titleServices]}>
+              Additional Message
+            </Text>
+            <TextInput
+              style={styles.textArea}
+              multiline={true}
+              numberOfLines={4}
+              onChangeText={(text) => setBussinessAdditional(text)}
+              placeholder="Enter your text here"
+            />
+          </View>
+          <View style={styles.submit}>
+            <TouchableOpacity>
+              <LinearGradient
+                colors={["#CEDC39", "#7DA74D"]}
+                style={styles.button}
+              >
+                <Text style={styles.button__text}>Appoinment a Service</Text>
+              </LinearGradient>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <Modal style={{ padding: 0, margin: 0 }} isVisible={isModalVisible}>
+          <View style={{ flex: 1, backgroundColor: "#fff" }}>
+            {origin && (
+              <MapView
+                style={{ flexGrow: 1 }}
+                initialRegion={{
+                  latitude: origin.latitude,
+                  longitude: origin.longitude,
+                  latitudeDelta: 0.005,
+                  longitudeDelta: 0.005,
+                }}
+                onPress={handleMapPress}
+              >
+                <Marker
+                  draggable
+                  coordinate={origin}
+                  onDragEnd={(direction) =>
+                    setOrigin(direction.nativeEvent.coordinate)
+                  }
+                />
+              </MapView>
+            )}
+            {!origin && (
+              <View
+                style={{
+                  flex: 1,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <ActivityIndicator size="large" color="#CEDC39" />
+              </View>
+            )}
+            <TouchableOpacity
+              onPress={toggleModal}
+              style={{ position: "absolute", top: 40, right: 20 }}
+            >
+              <StyledText>Close</StyledText>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+      </View>
+    </ScrollView>
+  );
+
+  const paintingRut = () => (
+    <View style={styles.Container}>
+      <Text>add paintingRut</Text>
+    </View>
+  );
+
+  const polishingRut = () => (
+    <View style={styles.Container}>
+      <Text>add polishingRut</Text>
+    </View>
+  );
+
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    { key: "cleaning", title: "cleaning" },
+    { key: "painting", title: "painting" },
+    { key: "polishing", title: "polishing" },
+  ]);
+
+  const renderScene = SceneMap({
+    cleaning: cleaningRut,
+    painting: paintingRut,
+    polishing: polishingRut,
+  });
 
   return (
     <>
-      <Header goBack={true} title={"Create a Service"} subtitle={"Create of the generated services"} />
+      <Header
+        title={"Create a Service"}
+        subtitle={"Schedule a service with us"}
+        goBack={true}
+      />
 
-      <ScrollView>
-        <View>
-          <View style={{ marginTop: 20, paddingHorizontal: 24 }}>
-            <Text style={[styles.titleServices]}>
-              Set date & time
-            </Text>
-            <TouchableOpacity style={styles.item}>
-              <View style={{ width: 40, height: 40, marginRight: 10 }}>
-                {getIconById("calendar")}
-              </View>
-              <Text>Choose a date</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.item}>
-              <View style={{ width: 40, height: 40, marginRight: 10 }}>
-                {getIconById("calendar")}
-              </View>
-              <Text>Schedule</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={[styles.item]}>
-              <View
-                style={{
-                  width: 30,
-                  height: 30,
-                  marginRight: 15,
-                  marginLeft: 7,
-                }}
-              >
-                {getIconById("iconLupa")}
-              </View>
-              <Text>Search by name or id employee</Text>
-            </TouchableOpacity>
-            <View style={styles.employees}>
-              <Text style={[styles.titleServices, { marginBottom: 5 }]}>
-                Employees
-              </Text>
-            </View>
-            <Text style={[styles.textGray, styles.titleServices]}>
-              Service Location
-            </Text>
-            <View style={[styles.item, stylesGlobal.itemVertical]}>
-              <Text>{selectedAddress}</Text>
-              <TouchableOpacity onPress={toggleModal}>
-                <Text style={styles.street__googleMaps}>SET GOOGLE MAP</Text>
-              </TouchableOpacity>
-            </View>
-            <View style={styles.bussiness}>
-              <Text style={styles.bussiness__title}>Bussiness Details</Text>
-              <Text style={[styles.textGray, styles.titleServices]}>
-                Bussiness Name
-              </Text>
-              <Text style={styles.bussiness__name}>{bussinessName}</Text>
-
-              <Text style={[styles.textGray, styles.titleServices]}>
-                Additional Message
-              </Text>
-              <TextInput
-                style={styles.textArea}
-                multiline={true}
-                numberOfLines={4}
-                onChangeText={(text) => setBussinessAdditional(text)}
-                placeholder="Enter your text here"
-              />
-            </View>
-            <View style={styles.submit}>
-              <TouchableOpacity>
-                <LinearGradient
-                  colors={["#CEDC39", "#7DA74D"]}
-                  style={styles.button}
-                >
-                  <Text style={styles.button__text}>Appoinment a Service</Text>
-                </LinearGradient>
-              </TouchableOpacity>
-            </View>
-          </View>
-          <Modal style={{ padding: 0, margin: 0 }} isVisible={isModalVisible}>
-            <View style={{ flex: 1, backgroundColor: "#fff" }}>
-              {origin && (
-                <MapView
-                  style={{ flexGrow: 1 }}
-                  initialRegion={{
-                    latitude: origin.latitude,
-                    longitude: origin.longitude,
-                    latitudeDelta: 0.005,
-                    longitudeDelta: 0.005,
-                  }}
-                  onPress={handleMapPress}
-                >
-                  <Marker
-                    draggable
-                    coordinate={origin}
-                    onDragEnd={(direction) =>
-                      setOrigin(direction.nativeEvent.coordinate)
-                    }
-                  />
-                </MapView>
-              )}
-              {!origin && (
-                <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-                  <ActivityIndicator size="large" color="#CEDC39" />
-                </View>
-              )}
-              <TouchableOpacity
-                onPress={toggleModal}
-                style={{ position: "absolute", top: 40, right: 20 }}
-              >
-                <StyledText>Close</StyledText>
-              </TouchableOpacity>
-            </View>
-          </Modal>
-        </View>
-      </ScrollView>
+      <TabView
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        initialLayout={initialLayout}
+        renderTabBar={(props) => (
+          <TabBar
+            {...props}
+            indicatorStyle={{ backgroundColor: "transparent", height: 0 }}
+            tabStyle={{ paddingLeft: 30 }}
+            style={{
+              backgroundColor: "#fff",
+              elevation: 0,
+              shadowOpacity: 0,
+              borderBottomWidth: 0,
+            }}
+            pressColor="transparent"
+            pressOpacity={1}
+            renderLabel={({ route, focused, color }) => (
+              <StyledGradientButtonSmall text={route.title} focused={focused} />
+            )}
+          />
+        )}
+      />
     </>
   );
 }
--- src/screens/Global/styles/CreateService.style.js
+++ src/screens/Global/styles/CreateService.style.js
@@ -2,131 +2,129 @@ import { StyleSheet } from "react-native";
 import { Color, fontFamily } from "../../../utils/constantsStyle";
 
 export const styles = StyleSheet.create({
-
- 
-   header: {
-      height: 155,
-      display: "flex",
-      flexDirection: "row",
-      alignItems: "center",
-      width: "100%",
-      paddingLeft: 20,
-      borderBottomLeftRadius: 25,
-      borderBottomRightRadius: 25
-   },
-   header__content: {
-      display: "flex",
-      flexDirection: "row",
-      alignItems: "center",
-   },
-   title: {
-      fontFamily: "Poppins_600SemiBold",
-      color: "#fff",
-      fontSize: 24,
-      lineHeight: 25
-   },
-   subTitle: {
-      fontFamily: "Poppins_400Regular",
-      color: "#fff",
-      fontSize: 15
-   },
-   textGray: {
-      color: "#828282"
-   },
-   titleServices: {
-      fontFamily: fontFamily.fontMedium,
-      fontSize: 17,
-      color:Color.gray1,
-   },
-   flatListContainer: {
-      flex:1,
-      marginBottom:20,
-      paddingHorizontal:24
-   },
-   item: {
-      display: "flex",
-      flexDirection: "row",
-      alignItems: "center",
-      height: 60,
-      position: "relative"
-   },
-   employees: {
-      width: "100%",
-      marginTop: 10,
-      marginBottom: 15
-   },
-   street: {
-      flex: 1,
-      paddingRight: 20
-   },
-   street__googleMaps: {
-      fontFamily: "Poppins_400Regular",
-      color: "#7DA74D",
-      textDecorationLine: "underline"
-   },
-   street__label: {
-      fontFamily: "Poppins_400Regular",
-   },
-   bussiness: {
-      marginTop: 10,
-      marginBottom: 15
-   },
-   bussiness__title: {
-      fontFamily: 'Poppins_700Bold',
-      fontSize: 19,
-      marginBottom: 10
-   },
-   bussiness__name: {
-      fontFamily: 'Poppins_400Regular',
-      padding: 10,
-      fontSize: 15
-   },
-   bussiness__additional: {
-      fontFamily: 'Poppins_400Regular',
-      padding: 10,
-      fontSize: 15
-   },
-   textArea: {
-      height: 100,
-      justifyContent: 'flex-start',
-      textAlignVertical: 'top',
-      borderColor: '#828282',
-      borderWidth: 1,
-      padding: 10,
-   },
-   submit: {
-      width: "100%",
-      display: "flex",
-      justifyContent: "center",
-      flexDirection: "row"
-   },
-   button: {
-      position: "relative",
-      width: 240,
-      height: 50,
-      borderRadius: 25,
-      marginBottom: 70,
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      marginTop: 20
-   },
-   button__text: {
-      fontFamily: 'Poppins_400Regular',
-      fontSize: 16,
-      color: "#fff"
-   },
-   input: {
-      backgroundColor: "transparent",
-      height: 40,
-      fontSize: 14,
-      fontWeight: "500",
-      color:Color.gray3,
-      borderRadius: 50,
-      marginLeft: 5,
-      display:"flex",
-      paddingLeft:20,
-      justifyContent:"center",
-      width:200
-    },
+  header: {
+    height: 155,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    paddingLeft: 20,
+    borderBottomLeftRadius: 25,
+    borderBottomRightRadius: 25,
+  },
+  header__content: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  title: {
+    fontFamily: "Poppins_600SemiBold",
+    color: "#fff",
+    fontSize: 24,
+    lineHeight: 25,
+  },
+  subTitle: {
+    fontFamily: "Poppins_400Regular",
+    color: "#fff",
+    fontSize: 15,
+  },
+  textGray: {
+    color: "#828282",
+  },
+  titleServices: {
+    fontFamily: fontFamily.fontMedium,
+    fontSize: 17,
+    color: Color.gray1,
+  },
+  flatListContainer: {
+    flex: 1,
+    marginBottom: 20,
+    paddingHorizontal: 24,
+  },
+  item: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    height: 60,
+    position: "relative",
+  },
+  employees: {
+    width: "100%",
+    marginTop: 10,
+    marginBottom: 15,
+  },
+  street: {
+    flex: 1,
+    paddingRight: 20,
+  },
+  street__googleMaps: {
+    fontFamily: "Poppins_400Regular",
+    color: "#7DA74D",
+    textDecorationLine: "underline",
+  },
+  street__label: {
+    fontFamily: "Poppins_400Regular",
+  },
+  bussiness: {
+    marginTop: 10,
+    marginBottom: 15,
+  },
+  bussiness__title: {
+    fontFamily: "Poppins_700Bold",
+    fontSize: 19,
+    marginBottom: 10,
+  },
+  bussiness__name: {
+    fontFamily: "Poppins_400Regular",
+    padding: 10,
+    fontSize: 15,
+  },
+  bussiness__additional: {
+    fontFamily: "Poppins_400Regular",
+    padding: 10,
+    fontSize: 15,
+  },
+  textArea: {
+    height: 100,
+    justifyContent: "flex-start",
+    textAlignVertical: "top",
+    borderColor: "#828282",
+    borderWidth: 1,
+    padding: 10,
+  },
+  submit: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+  button: {
+    position: "relative",
+    width: 240,
+    height: 50,
+    borderRadius: 25,
+    marginBottom: 70,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
+  },
+  button__text: {
+    fontFamily: "Poppins_400Regular",
+    fontSize: 16,
+    color: "#fff",
+  },
+  input: {
+    backgroundColor: "transparent",
+    height: 40,
+    fontSize: 14,
+    fontWeight: "500",
+    color: Color.gray3,
+    borderRadius: 50,
+    marginLeft: 5,
+    display: "flex",
+    paddingLeft: 20,
+    justifyContent: "center",
+    width: 200,
+  },
 });
